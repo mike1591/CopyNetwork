b9b73729687884745676e4807e8ccb7df0b38553
Mike Yushkov <43010402+mike1591@users.noreply.github.com>
2023-09-27 20:53:57 +0700

    json -  конфмгурация разработка с использованием ZIP

---
+++ test.bat
@@ -0,0 +1,3 @@
+oscript.exe TaskCopy.os genjson "D:\Project\CopyNetwork\conf.json"
+oscript.exe TaskCopy.os genkey "D:\Project\CopyNetwork\conf.json" "D:\Project\CopyNetwork\conf.key"
+oscript.exe TaskCopy.os work "D:\Project\CopyNetwork\conf.key"
